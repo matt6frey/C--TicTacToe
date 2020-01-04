3896bbf2ccc36f62a9da2ec5b0dcadea40e2e5d3
Matt Frey <matt6frey@gmail.com>
2020-01-04 11:44:34 -0700

    Complete TicTacToe console game.

---
+++ Game.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Game
+    {
+        public string player1Name;
+        public int player1Wins = 0;
+        public string player1Token = "X";
+        public string player2Name;
+        public string player2Token = "O";
+        public int player2Wins = 0;
+        public Game()
+        {
+            player1Name = GetName(1);
+            player2Name = GetName(2);
+        }
+
+        public void Play()
+        {
+            GameBoard Board = new GameBoard();
+            string[] players = { player1Name, player2Name };
+            string[] tokens = { player1Token, player2Token };
+
+            while (Board.winner.Equals(""))
+            {
+                Board.Display();
+                Console.WriteLine($"It's your turn, Player {players[Board.playerTurn]}");
+                Board.UpdateBoard(GetInput(Board));
+                Console.Clear();
+
+                foreach (var token in tokens)
+                {
+                    Board.HasWinner(token);
+                }
+                Board.ChangeTurn();
+                if (Board.IsDraw()) { 
+                    Board.winner = "DRAW";
+                    Console.WriteLine("It's a draw!");
+                }
+            }
+            if (!Board.IsDraw())
+            {
+                string winner = (Board.winner.Equals(player1Token)) ? player1Name : player2Name;
+                player1Wins += (Board.winner.Equals(player1Token)) ? 1 : 0;
+                player2Wins += (Board.winner.Equals(player2Token)) ? 1 : 0;
+                AnnounceWinner(winner);
+            }
+            Board.Setup();
+            PlayAgain();
+        }
+
+        public int GetInput(GameBoard Board)
+        {
+            string input = "";
+            int number = 0;
+            while (input.Equals(""))
+            {
+                input = Console.ReadLine();
+                bool isAcceptable = int.TryParse(input, out number);
+
+                if (number > 9 || !isAcceptable || !Board.TileIsOpen(input))
+                {
+                    Console.WriteLine((!Board.TileIsOpen(input)) ? "Choose a different number, that tile is chosen.: " : "Please enter a digit from 1 - 9: ");
+                    input = "";
+                }
+            }
+            return number - 1;
+        }
+
+        public string GetName(int player)
+        {
+            Console.WriteLine($"Enter Name for Player {player}: ");
+            return Console.ReadLine();
+        }
+
+        public void AnnounceWinner(string player)
+        {
+            Console.WriteLine($"\n{player} wins!\n");
+        }
+        
+        public void PlayAgain()
+        {
+            Console.WriteLine("How about another round? (Y/N)");
+            string input = Console.ReadLine().ToUpper();
+            if (input.Contains("YES") || input.Equals("Y"))
+            {
+                Console.Clear();
+                Play();
+            }
+            else
+            {
+                Console.WriteLine("Thanks for playing!\n");
+                Console.WriteLine($"Player {player1Name} wins: {player1Wins}");
+                Console.WriteLine($"Player {player2Name} wins: {player2Wins}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ GameBoard.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+
+namespace TicTacToe
+{
+    class GameBoard
+    {
+        public string[] tiles;
+        public int[] closedTiles;
+        public string winner = "";
+        public int playerTurn;
+        public int turnCount = 0;
+
+        public GameBoard()
+        {
+            Setup();
+        }
+
+        public void Setup()
+        {
+            winner = "";
+            playerTurn = 0;
+            closedTiles = SetTiles();
+            tiles = CreateBoard();
+        }
+
+        public int[] SetTiles()
+        {
+            int[] closedTiles = { -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+            return closedTiles;
+        }
+
+        public string[] CreateBoard()
+        {
+            string[] tiles = { "1","2","3","4","5","6","7","8","9" };
+            return tiles;
+        }
+
+        public void Display()
+        {
+            for (int i = 0; i < 9; i += 3)
+            {
+                Console.WriteLine("-------------");
+                Console.WriteLine($"| {tiles[i]} | {tiles[i + 1]} | {tiles[i + 2]} |");
+            }
+            Console.WriteLine("-------------\n\n");
+        }
+
+        public void ChangeTurn()
+        {
+            playerTurn += (playerTurn == 0) ? 1 : -1;
+        }
+
+        public bool TileIsOpen(string choice)
+        {
+            int tile = (int.TryParse(choice, out int number)) ? number : -1;
+            if (tile != -1 && !closedTiles.Contains(tile))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void UpdateBoard(int choice)
+        {
+            string playerToken = (playerTurn == 0) ? "X" : "O";
+            tiles[choice] = playerToken;
+            closedTiles[turnCount] = choice + 1;
+            turnCount++;
+        }
+
+        public bool HasWinner(string target)
+        {
+            if ((tiles[0] == target && tiles[1] == target && tiles[2] == target)
+                    || (tiles[3] == target && tiles[4] == target && tiles[5] == target)
+                    || (tiles[6] == target && tiles[7] == target && tiles[8] == target)
+                    || (tiles[0] == target && tiles[3] == target && tiles[6] == target)
+                    || (tiles[1] == target && tiles[4] == target && tiles[7] == target)
+                    || (tiles[2] == target && tiles[5] == target && tiles[8] == target)
+                    || (tiles[0] == target && tiles[4] == target && tiles[8] == target)
+                    || (tiles[2] == target && tiles[4] == target && tiles[6] == target))
+            {
+                winner = target;
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsDraw()
+        {
+            return turnCount == 9 && (!winner.Equals("X") || !winner.Equals("O"));
+        }
+    }
+}
